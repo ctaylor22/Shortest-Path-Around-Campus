d8f27a4528c768a999ea7a0409b8d8b6232b7677
Conner Taylor <ctaylor20@georgefox.edu>
2022-04-02 12:26:12 -0700

    print

--- static/front-end.js
+++ static/front-end.js
@@ -1,9 +1,9 @@
 'use strict';
 (function()
 {
-    function printOutSomething()
+    function printOutSomething(x)
     {
-        document.write(5 + 6)
+        document.write(x)
     }
 
     function getRequest(url)
@@ -13,10 +13,14 @@
         request.send()
         request.onload = function() {
           if (this.status >= 200 && this.status < 400) {
-            printOutSomething()
+            printOutSomething(JSON.parse(this.response))
           }
         }
     }
 
-    document.addEventListener('dblclick', printOutSomething);
+    document.addEventListener('dblclick', function()
+    {
+        let url = "/api/v1/"
+        getRequest(url);
+    });
 }())
