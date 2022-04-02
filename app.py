8771f6d8d8dc6fe93ed0b153f85c6f590a93c29d
Conner Taylor <ctaylor20@georgefox.edu>
2022-04-02 02:04:35 -0700

    Change

--- app.py
+++ app.py
@@ -2,6 +2,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
