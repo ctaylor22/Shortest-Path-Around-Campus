4a0af4e88ba4dd0923ef0097a9a300ea099ea7e8
Conner Taylor <ctaylor20@georgefox.edu>
2022-04-02 13:21:20 -0700

    dd

--- static/front-end.js
+++ static/front-end.js
@@ -13,7 +13,7 @@
         request.send()
         request.onload = function() {
           if (this.status >= 200 && this.status < 400) {
-            printOutSomething(JSON.parse(this.response['Yay']))
+            printOutSomething(this.response)
           }
         }
     }
