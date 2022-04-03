3571d9d52052cf1f2b901e281ad2ccf4eb6181a7
Conner Taylor <ctaylor20@georgefox.edu>
2022-04-02 20:54:20 -0700

    add label to feet

--- static/front-end.js
+++ static/front-end.js
@@ -28,7 +28,7 @@
         path = path.replace(/['"]+/g, "");
         shortestPath = path.split(",");
         // path_len = shortestPath[shortestPath.length - 1];
-        document.getElementById("h1").innerHTML = shortestPath[shortestPath.length - 1];
+        document.getElementById("h1").innerHTML = "Feet: " + shortestPath[shortestPath.length - 1];
         shortestPath.pop();
 
         for (let i = 0; i < shortestPath.length; i++)
