308b070418e9bca017e1f9cd95e67baad188f157
Conner Taylor <ctaylor20@georgefox.edu>
2022-04-02 19:04:14 -0700

    r bigger

--- static/front-end.js
+++ static/front-end.js
@@ -11,8 +11,8 @@
         for (let i = 0; i < shortestPath.length; i++)
         {
             let station = document.getElementById(shortestPath[i]);
-            // let radius = parseFloat(station.getAttribute('r')) + 2;
-            // station.setAttribute('r', radius);
+            let radius = parseFloat(station.getAttribute('r')) + 2;
+            station.setAttribute('r', radius);
             station.setAttribute('fill', "#FAED27");
         }
     }
